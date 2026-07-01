41c661800c49eef46c75f407585511786caa1e26
Tokyo-Fiaz <tylerredeaufrench@gmail.com>
2026-07-01 15:58:42 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+print("Student Life CLI Tracker")
+print("------------------------")
+print("Welcome! Choose an option from the menu below.")
+
+print("1. Add a task")
+print("2. View all tasks")
+print("3. Mark a task complete")
+print("4. Delete a task")
+print("5. Exit")
